aab1265878801c88cd58ceb7765a83019a8c6f56
aabirsark <factbomberz.1@gmail.com>
2021-08-09 15:38:51 +0530

    added add data screen

---
+++ lib/screens/home/widgets/root_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeRootWidget extends StatelessWidget {
+  const HomeRootWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [],
+    );
+  }
+}
+
+class SavingsCard extends StatelessWidget {
+  const SavingsCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
